68748e0a1cafb43fe219c207303d32eb235c7f2a
Mu Ye Liu <muyeliu1208@gmail.com>
2025-05-20 14:00:20 -0700

    created the react part

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          NP-Complete Date puzzle solver
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Np complete date puzzle solver
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
